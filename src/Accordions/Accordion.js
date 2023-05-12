41b52109c016a194765c737c5540c7ab97793e03
navya <navya@metrictreelabs.com>
2023-05-13 01:31:33 +0530

    accordion completed

--- src/Accordions/Accordion.js
+++ src/Accordions/Accordion.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Accordion() {
+export default function Accordion({accordionData}) {
+  const [accordionIds,setAccordionIds]=useState([])
+  const handlerAccordion=(id)=>
+  {
+    setAccordionIds((prev)=>
+    {
+     return accordionIds.includes(id)?
+      accordionIds.filter((accid)=>accid!==id):[...prev,id];
+    })
+  }
   return (
-    <div>Accordion</div>
+    <>
+    { accordionData && accordionData.map(({id,title,description})=>
+   (
+    <>
+    <div className='accordion' key={id}>
+    <div className='accordion-title' onClick={()=>handlerAccordion(id)}>
+      <h3>
+{title}
+      </h3>
+    </div>
 
-
-
+{
+  accordionIds?.map((acc_id)=>
+  (
+    acc_id ===id &&
+ ( <div className='accordion-description'>
+  <h3>
+  {description}
+  </h3>
+</div>)
+)
+  )
+}
     
-    
+  </div>
+  </>
+   )
+    )}
+    </>
   )
 }
--- src/Accordions/AccordionData.js
+++ src/Accordions/AccordionData.js
@@ -2,16 +2,17 @@ const accordionData=[
     {
         id:111,
         title:"first",
-        description:"first component"
+        description:"first component This command creates an empty Git repository - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files. An initial branch without any commits will be created (see the --initial-branch option below for its name)."
     },
     {
         id:112,
         title:"second",
-        description:"second component"
+        description:"second component This command creates an empty Git repository - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files. An initial branch without any commits will be created (see the --initial-branch option below for its name)."
     },
     {
         id:113,
         title:"third",
-        description:"third component"
+        description:"third component This command creates an empty Git repository - basically a .git directory with subdirectories for objects, refs/heads, refs/tags, and template files. An initial branch without any commits will be created (see the --initial-branch option below for its name)."
     }
 ]
+export default accordionData;
