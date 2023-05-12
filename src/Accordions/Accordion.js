1a3043a65d7536dfbaf292a50e1e138df5a83673
navya <navya@metrictreelabs.com>
2023-05-12 16:01:54 +0530

    first commit

---
+++ src/Accordions/Accordion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Accordion() {
+  return (
+    <div>Accordion</div>
+
+
+
+    
+    
+  )
+}
---
+++ src/Accordions/AccordionData.js
@@ -0,0 +1,17 @@
+const accordionData=[
+    {
+        id:111,
+        title:"first",
+        description:"first component"
+    },
+    {
+        id:112,
+        title:"second",
+        description:"second component"
+    },
+    {
+        id:113,
+        title:"third",
+        description:"third component"
+    }
+]
